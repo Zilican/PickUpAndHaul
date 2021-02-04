65829a9fe65c1590bdc7920d82d6a76de69f0ccb
Zilican <78545667+Zilican@users.noreply.github.com>
2021-02-04 14:05:28 +0000

    Update JobDriver_HaulToInventory.cs

--- Source/PickUpAndHaul/JobDriver_HaulToInventory.cs
+++ Source/PickUpAndHaul/JobDriver_HaulToInventory.cs
@@ -154,45 +154,7 @@ namespace PickUpAndHaul
             yield return wait;
         }
 
-        public Toil CheckForOverencumbered()
-        {
-            var toil = new Toil();
-            toil.initAction = delegate
-            {
-                Pawn actor = toil.actor;
-                Job curJob = actor.jobs.curJob;
-                Thing nextThing = curJob.targetA.Thing;
-
-                //float usedBulkByPct = 1f;
-                //float usedWeightByPct = 1f;
-
-                //try
-                //{
-                //    ((Action)(() =>
-                //    {
-                //        if (ModCompatibilityCheck.CombatExtendedIsActive)
-                //        {
-                //            CompInventory ceCompInventory = actor.GetComp<CompInventory>();
-                //            usedWeightByPct = ceCompInventory.currentWeight / ceCompInventory.capacityWeight;
-                //            usedBulkByPct = ceCompInventory.currentBulk / ceCompInventory.capacityBulk;
-                //        }
-                //    }))();
-                //}
-                //catch (TypeLoadException) { }
-
-
-                if (!(MassUtility.EncumbrancePercent(actor) <= 0.9f /*|| usedBulkByPct >= 0.7f || usedWeightByPct >= 0.8f*/))
-                {
-                    Job haul = HaulAIUtility.HaulToStorageJob(actor, nextThing);
-                    if (haul?.TryMakePreToilReservations(actor, false) ?? false)
-                    {
-                        //note that HaulToStorageJob etc doesn't do opportunistic duplicate hauling for items in valid storage. REEEE
-                        actor.jobs.jobQueue.EnqueueFirst(haul, JobTag.Misc);
-                        EndJobWith(JobCondition.Succeeded);
-                    }
-                }
-            };
-            return toil;
-        }
+        
+        
     }
 }
